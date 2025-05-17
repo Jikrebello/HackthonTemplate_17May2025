d0ad0a5251970ef41e35f4b086dd65f3abf9dad6
James Rebello <Jikrebello.za@gmail.com>
2025-05-17 06:32:30 +0200

    Refactor application to use Blazor WebAssembly and Fluent UI
    This commit includes significant changes to the web application, transitioning from a server-side model to Blazor WebAssembly. Key updates involve the removal of legacy components such as Bootstrap, WeatherForecast functionality, and error handling pages. The project now utilizes Fluent UI components for the layout and navigation, and new package references have been added for Blazored.LocalStorage. Additionally, various CSS files have been updated or removed to align with the new design, and the project structure has been reorganized to reflect these changes.

--- MyApp.API/Program.cs
+++ MyApp.API/Program.cs
@@ -4,7 +4,21 @@ using MyApp.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        policy =>
+        {
+            policy
+                .SetIsOriginAllowed(_ => true)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        }
+    );
+});
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -18,15 +32,15 @@ using (var scope = app.Services.CreateScope())
     await DbSeeder.SeedRolesAsync(roleManager);
 }
 
-// Configure the HTTP request pipeline
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
--- MyApp.Web/Program.cs
+++ MyApp.Web/Program.cs
@@ -1,34 +1,19 @@
-using Microsoft.AspNetCore.Components;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
-using MudBlazor.Services;
-using MyApp.Web.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using MyApp.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-builder.Services.AddMudServices();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+builder.Services.AddScoped(sp => new HttpClient
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
+builder.Services.AddFluentUIComponents();
 
-app.UseHttpsRedirection();
+builder.Services.AddBlazoredLocalStorage();
 
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+await builder.Build().RunAsync();
